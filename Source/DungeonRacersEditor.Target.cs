39cf0c8df3ba97e75caf7434c2ca25400681df9b
leehyvaa <k05n17j@gmail.com>
2024-08-16 08:04:46 +0900

    버전변경

--- Source/DungeonRacersEditor.Target.cs
+++ Source/DungeonRacersEditor.Target.cs
@@ -1,15 +1,13 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class DungeonRacersEditorTarget : TargetRules
 {
-	public DungeonRacersEditorTarget( TargetInfo Target) : base(Target)
+	public DungeonRacersEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("DungeonRacers");
 	}
 }
